31b3bffcaec01d14a36be490c9a6097259dd165c
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2023-03-20 19:53:17 +0100

    Streamline colors, add state mgmt to create.tsx

--- coordinator-frontend/src/components/navbar.tsx
+++ coordinator-frontend/src/components/navbar.tsx
@@ -1,34 +1,35 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-const navigationItems = [
+/* These will be used to populate the left-aligned navigation bar item list. */
+const navPathList = [
     { name: 'Hírek', path: '/news' },
     { name: 'Új bejegyzés', path: '/news/create' }
 ];
 
 export default function Navbar() {
-    const activeClasses = 'text-gray-100 bg-gray-700 ';
-    const inactiveClasses = 'text-gray-700 ';
-    const commonClasses = 'hover:outline hover:outline-3 hover:outline-gray-700 px-6 py-3 mx-2 text-lg ';
+    /* Tailwind styling classes. */
+    const selected = 'hover:outline hover:outline-3 hover:outline-stone-700 px-6 py-3 mx-2 text-stone-100 bg-stone-700';
+    const inactive = 'hover:outline hover:outline-3 hover:outline-stone-700 px-6 py-3 mx-2';
 
-    const { asPath } = useRouter()
+    /* Apply different classes to nav items based on current path. */
+    const { asPath } = useRouter();
+    const links = navPathList.map((item, i) =>
+        <Link href={item.path} key={i} className={(asPath === item.path) ? selected : inactive}>{item.name}</Link>
+    );
 
-    const navElements = navigationItems.map((item, i) => {
-        const classes = (asPath === item.path) ? (commonClasses + activeClasses) : (commonClasses + inactiveClasses);
-        return <Link href={item.path} key={i} className={classes}>{item.name}</Link>
-    });
-
+    /* Generate header markup. */
     return (
         <header>
-            <nav className='flex items-center px-4 py-5 bg-gray-100'>
-                <div className='text-gray-700 text-lg ml-2 mr-6'>
+            <nav className='flex items-center text-stone-700 bg-stone-100 text-lg px-4 py-5'>
+                <div className='ml-2 mr-6'>
                     <span>Event Coordinator</span>
                 </div>
-                <div className='gap-x-12 mr-auto'>
-                    {navElements}
+                <div className='mr-auto'>
+                    {links}
                 </div>
                 <div>
-                    <Link href='/' className={commonClasses + inactiveClasses}>Bejelentkezés</Link>
+                    <Link href='/' className={inactive}>Bejelentkezés</Link>
                 </div>
             </nav>
         </header>
--- coordinator-frontend/src/pages/news/create.tsx
+++ coordinator-frontend/src/pages/news/create.tsx
@@ -1,20 +1,70 @@
 import Navbar from '@/components/navbar';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
+import { useState } from 'react';
 import 'react-quill/dist/quill.snow.css';
 
+/* QuillJS has to be loaded client-side, we can't render it on the server. */
 const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
 export default function NewsCreate() {
+    /* Tailwind styling classes. */
+    const primary = 'hover:outline hover:outline-3 hover:outline-stone-700 px-8 py-3 text-lg bg-stone-700 text-stone-100';
+    const secondary = 'hover:outline hover:outline-3 hover:outline-stone-700 px-8 py-3 text-lg text-stone-700 mr-4';
+
+    /* Initialize state management. */
+    const [text, setText] = useState('');
+    const [isPreview, setIsPreview] = useState(false);
+
+    /* Preview button click handler. */
+    const onPreviewClick = () => {
+        setIsPreview(value => !value);
+    };
+
+    /* Dummy save button click handler. */
+    const onSaveClickDummy = () => {
+        alert(text);
+    };
+
+    /* Customize QuillJS toolbar module. */
+    const modules = {
+        toolbar: [
+            [{'font': []}],
+
+            ['bold', 'italic', 'underline'],
+            [{'color': []}, {'background': []}],
+            ['link', 'image'],
+            [{'list': 'ordered'}, {'list': 'bullet'}],
+            ['blockquote', {'indent': '-1'}, {'indent': '+1'}],
+
+            ['clean']
+        ]
+    };
+
+    /* Generate page markup. */
     return (
         <>
             <Head>
-                <title>Coordinator</title>
+                <title>Új bejegyzés - Coordinator</title>
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
             </Head>
+            
             <Navbar />
-            <div className='flex-1 w-full flex items-center justify-center'>
-                <ReactQuill theme='snow' />
+
+            <div className='flex-1 container mx-auto px-4'>
+                <h1 className='text-stone-700 text-2xl mt-6 mb-4'>Új bejegyzés</h1>
+                <div className='h-3/5'>
+                    {
+                        isPreview
+                        ? <div dangerouslySetInnerHTML={{__html: text}}></div> /* TODO: This is unsafe (XSS). */
+                        : <ReactQuill theme='snow' className='h-full' value={text} onChange={setText}
+                                      modules={modules} placeholder='Bejegyzés szövege...' />
+                    }
+                </div>
+                <div className='flex justify-end mt-14 mb-4'>
+                    <button className={secondary} onClick={onPreviewClick}>Előnézet</button>
+                    <button className={primary} onClick={onSaveClickDummy}>Mentés</button>
+                </div>
             </div>
         </>
     );
