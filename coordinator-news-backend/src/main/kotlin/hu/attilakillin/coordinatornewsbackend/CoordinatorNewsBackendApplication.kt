a81beed643c411a5094522084873826feed04e0a
Killin Attila <43002846+attilakillin@users.noreply.github.com>
2023-03-30 11:16:46 +0200

    Initialize news service backend

---
+++ coordinator-news-backend/src/main/kotlin/hu/attilakillin/coordinatornewsbackend/CoordinatorNewsBackendApplication.kt
@@ -0,0 +1,11 @@
+package hu.attilakillin.coordinatornewsbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoordinatorNewsBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<CoordinatorNewsBackendApplication>(*args)
+}
